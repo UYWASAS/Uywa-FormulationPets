ee6766732f6f6846f5280cefffc8dfa12fbcbb45
UYWASAS <uywasas@gmail.com>
2025-08-05 15:34:18 -0500

    Update nutrient_reference.py

--- nutrient_reference.py
+++ nutrient_reference.py
@@ -1,5 +1,5 @@
 NUTRIENTES_REFERENCIA_PERRO = {
-    "Proteina":         {"min": 6.25, "max": None, "unit": "g/100g"},
+    "PB":         {"min": 6.25, "max": None, "unit": "g/100g"},
     "EM":               {"min": 1000, "max": 1100, "unit": "kcal/kg"},
     "Grasa":            {"min": 2.125, "max": None, "unit": "g/100g"},
     "Ácido Linoleico":  {"min": 0.325, "max": 1.625, "unit": "g/100g"},
